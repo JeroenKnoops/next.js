b94e0bdff7a04378a6dbfd962a0fea4a2a83283e
Jeroen Knoops <jeroen.knoops@philips.com>
2019-01-07 23:21:07 +0100

    :hammer: Refactor: Uses real code instead of dist

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   verbose: true,
 
-  testMatch: ['*/**/unit/*.test.js'],
+  testMatch: ['*/**/*.test.js'],
 
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx'],
 
--- test/lib/next-test-utils.js
+++ test/lib/next-test-utils.js
@@ -8,10 +8,7 @@ import spawn from 'cross-spawn'
 import { readFileSync, writeFileSync, existsSync, unlinkSync } from 'fs'
 import fkill from 'fkill'
 
-// `next` here is the symlink in `test/node_modules/next` which points to the root directory.
-// This is done so that requiring from `next` works.
-// The reason we don't import the relative path `../../dist/<etc>` is that it would lead to inconsistent module singletons
-import server from 'next/dist/server/next'
+import server from 'next/server/next'
 import _pkg from 'next/package.json'
 
 export const nextServer = server
@@ -41,7 +38,7 @@ export function initNextServerScript (scriptPath, successRegexp, env) {
       instance.stderr.removeListener('data', handleStderr)
     })
 
-    instance.on('error', (err) => {
+    instance.on('error', err => {
       reject(err)
     })
   })
@@ -53,11 +50,13 @@ export function renderViaAPI (app, pathname, query) {
 }
 
 export function renderViaHTTP (appPort, pathname, query) {
-  return fetchViaHTTP(appPort, pathname, query).then((res) => res.text())
+  return fetchViaHTTP(appPort, pathname, query).then(res => res.text())
 }
 
 export function fetchViaHTTP (appPort, pathname, query) {
-  const url = `http://localhost:${appPort}${pathname}${query ? `?${qs.stringify(query)}` : ''}`
+  const url = `http://localhost:${appPort}${pathname}${
+    query ? `?${qs.stringify(query)}` : ''
+  }`
   return fetch(url)
 }
 
@@ -69,7 +68,10 @@ export function runNextCommand (argv, options = {}) {
   const cwd = path.dirname(require.resolve('next/package'))
   return new Promise((resolve, reject) => {
     console.log(`Running command "next ${argv.join(' ')}"`)
-    const instance = spawn('node', ['dist/bin/next', ...argv], { cwd, stdio: options.stdout ? ['ignore', 'pipe', 'ignore'] : 'inherit' })
+    const instance = spawn('node', ['dist/bin/next', ...argv], {
+      cwd,
+      stdio: options.stdout ? ['ignore', 'pipe', 'ignore'] : 'inherit'
+    })
 
     let stdoutOutput = ''
     if (options.stdout) {
@@ -84,7 +86,7 @@ export function runNextCommand (argv, options = {}) {
       })
     })
 
-    instance.on('error', (err) => {
+    instance.on('error', err => {
       reject(err)
     })
   })
@@ -115,7 +117,7 @@ export function runNextCommandDev (argv, stdOut) {
       instance.stderr.removeListener('data', handleStderr)
     })
 
-    instance.on('error', (err) => {
+    instance.on('error', err => {
       reject(err)
     })
   })
@@ -130,7 +132,7 @@ export function nextBuild (dir, args = []) {
   return runNextCommand(['build', dir, ...args])
 }
 
-export function nextExport (dir, {outdir}) {
+export function nextExport (dir, { outdir }) {
   return runNextCommand(['export', dir, '--outdir', outdir])
 }
 
@@ -171,7 +173,7 @@ function promiseCall (obj, method, ...args) {
 }
 
 export function waitFor (millis) {
-  return new Promise((resolve) => setTimeout(resolve, millis))
+  return new Promise(resolve => setTimeout(resolve, millis))
 }
 
 export async function startStaticServer (dir) {
@@ -193,9 +195,9 @@ export async function check (contentFn, regex) {
     try {
       content = await contentFn()
     } catch (err) {
-      console.error('Error while getting content', {regex})
+      console.error('Error while getting content', { regex })
     }
-    console.error('TIMED OUT CHECK: ', {regex, content})
+    console.error('TIMED OUT CHECK: ', { regex, content })
     throw new Error('TIMED OUT: ' + regex + '\n\n' + content)
   }, 1000 * 30)
   while (!found) {
@@ -214,7 +216,9 @@ export async function check (contentFn, regex) {
 export class File {
   constructor (path) {
     this.path = path
-    this.originalContent = existsSync(this.path) ? readFileSync(this.path, 'utf8') : null
+    this.originalContent = existsSync(this.path)
+      ? readFileSync(this.path, 'utf8')
+      : null
   }
 
   write (content) {
@@ -258,12 +262,16 @@ export async function getReactErrorOverlayContent (browser) {
       }
 
       found = true
-      return browser.eval(`document.querySelector('iframe').contentWindow.document.body.innerHTML`)
+      return browser.eval(
+        `document.querySelector('iframe').contentWindow.document.body.innerHTML`
+      )
     } catch (ex) {
       await waitFor(1000)
     }
   }
-  return browser.eval(`document.querySelector('iframe').contentWindow.document.body.innerHTML`)
+  return browser.eval(
+    `document.querySelector('iframe').contentWindow.document.body.innerHTML`
+  )
 }
 
 export function getBrowserBodyText (browser) {
